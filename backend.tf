c7f150ce66a51b4f46ec2cb2bcf3e76febba8e18
kondamani <m.kondamani1985@gmail.com>
2025-07-23 10:29:41 +0530

    adding var and provider tf file

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "manis3bucket"
     key = "eks/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "mani_lockfile"
     encrypt = true
     
   }
